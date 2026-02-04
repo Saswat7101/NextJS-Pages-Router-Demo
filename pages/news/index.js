75e4debb5dde4651c44e96f7eed573828ea764cc
Saswat <mohantysaswat154@gmail.com>
2026-02-04 10:26:21 +0530

    linking between pages

--- pages/news/index.js
+++ pages/news/index.js
@@ -1,5 +1,21 @@
 // our-domain.com/news
 
+import Link from "next/link";
+
 export default function NewsPage() {
-  return <h1>News Page</h1>;
+  return (
+    <>
+      <h1>News Page</h1>
+      <ul>
+        <li>
+          <Link href="/news/next-js-is-a-great-framework">
+            NextJS is a Great Framework
+          </Link>
+        </li>
+        <li>
+          <Link href="/news/something-else">Something Else</Link>
+        </li>
+      </ul>
+    </>
+  );
 }
